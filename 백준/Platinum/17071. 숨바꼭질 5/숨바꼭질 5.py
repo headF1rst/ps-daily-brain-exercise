10b806f78f782f6c527069dea7486152619aa763
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-01-17 02:00:55 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 424 ms, Memory: 192900 KB -BaekjoonHub

---
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, k = map(int, input().split())
+Max = 500000
+visited = [[-1] * 2 for _ in range(Max + 1)]
+
+def in_boundary(nx):
+    return 0 <= nx <= Max
+
+def bfs():
+    q = deque([(n, 0)])
+    visited[n][0] = 0
+    while q:
+        x, cnt = q.popleft()
+        flag = cnt % 2
+        for nx in [x + 1, x - 1, x * 2]:
+            if in_boundary(nx) and visited[nx][1 - flag] == -1:
+                visited[nx][1 - flag] = cnt + 1
+                q.append((nx, cnt + 1))
+
+bfs()
+time, flag, answer = 0, 0, -1
+while k <= Max:
+    if visited[k][flag] != -1 and visited[k][flag] <= time:
+        answer = time
+        break
+    flag = 1 - flag
+    time += 1
+    k += time
+
+print(answer)

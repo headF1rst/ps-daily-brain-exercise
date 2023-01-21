57049756f9a19633c3eefb1c97fafa840a0ff954
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-01-21 19:48:26 +0900

    [Gold IV] Title: 루머, Time: 808 ms, Memory: 204552 KB -BaekjoonHub

--- 백준/Gold/19538. 루머/루머.py
+++ 백준/Gold/19538. 루머/루머.py
@@ -9,23 +9,22 @@ starts = list(map(int, input().split()))
 answer = [-1] * (n + 1)
 turn = [0] * (n + 1)
 q = deque()
+t = 0
 
 for start in starts:
-    q.append(start)
     answer[start] = 0
+    q.append(start)
 
 for i in range(1, n + 1):
-    turn[i] = len(G[i]) // 2
+    turn[i] += len(G[i]) // 2
 
 while q:
     cur = q.popleft()
     for next_node in G[cur]:
-        if next_node == 0:
-            break
         turn[next_node] -= 1
         if answer[next_node] == -1 and turn[next_node] <= 0:
-            q.append(next_node)
             answer[next_node] = answer[cur] + 1
+            q.append(next_node)
 
 for i in range(1, n + 1):
     print(answer[i], end=' ')

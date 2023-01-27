ee74f6415320525840a255ee8b9b72f13ab40149
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-01-28 07:06:50 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 476 ms, Memory: 133852 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,15 +1,16 @@
-import math
-from heapq import heappush, heappop
+from heapq import heappop, heappush
 import math
 
 n, e = map(int, input().split())
-INF = math.inf
-G = [[INF] * (n + 1) for _ in range(n + 1)]
+G = [[] for _ in range(n + 1)]
 for _ in range(e):
     a, b, c = map(int, input().split())
-    G[a][b] = c
-    G[b][a] = c
+    G[a].append((b, c))
+    G[b].append((a, c))
+
 v1, v2 = map(int, input().split())
+INF = math.inf
+
 
 def dijk(source, target1, target2):
     dist = [INF] * (n + 1)
@@ -17,17 +18,19 @@ def dijk(source, target1, target2):
     pq = [(0, source)]
     while pq:
         cost, cur = heappop(pq)
-        for i in range(1, n + 1):
-            next_cost = dist[cur] + G[cur][i]
-            if next_cost < dist[i]:
-                dist[i] = next_cost
-                heappush(pq, (next_cost, i))
+        for next_node, c in G[cur]:
+            next_cost = c + dist[cur]
+            if next_cost < dist[next_node]:
+                dist[next_node] = next_cost
+                heappush(pq, (next_cost, next_node))
+
     return dist[target1], dist[target2]
 
 a, d = dijk(1, v1, v2)
 b, f = dijk(v1, v2, n)
 c, e = dijk(v2, n, v1)
 answer = min(a + b + c, d + e + f)
+
 if answer == INF:
     print(-1)
 else:

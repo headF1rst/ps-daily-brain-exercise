ffdb378c29248eec4984a5c4cee2dcd7befa8fc4
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-02-01 07:32:19 +0900

    [Platinum V] Title: 집배원 한상덕, Time: 1572 ms, Memory: 120868 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
@@ -4,11 +4,12 @@ from collections import deque
 n = int(input())
 G = [list(input()) for _ in range(n)]
 h = [list(map(int, input().split())) for _ in range(n)]
-dxs = [1, -1, 0, 0, 1, 1, -1, -1]
-dys = [0, 0, 1, -1, 1, -1, 1, -1]
 houses = 0
 fatigue = set()
-answer = math.inf
+INF = math.inf
+answer = INF
+dxs = [1, -1, 0, 0, 1, -1, 1, -1]
+dys = [0, 0, 1, -1, -1, 1, 1, -1]
 
 for i in range(n):
     for j in range(n):
@@ -19,17 +20,16 @@ for i in range(n):
         fatigue.add(h[i][j])
 
 fatigue = list(sorted(fatigue))
-
 s, e = 0, 0
 len_f = len(fatigue)
 
 def in_boundary(nx, ny):
     return 0 <= nx < n and 0 <= ny < n
 
-def bfs(sx, sy):
+def bfs(i, j):
     visited = [[False] * n for _ in range(n)]
-    q = deque([(sx, sy)])
-    visited[sx][sy] = True
+    visited[i][j] = True
+    q = deque([(i, j)])
     cnt = 0
     while q:
         x, y = q.popleft()

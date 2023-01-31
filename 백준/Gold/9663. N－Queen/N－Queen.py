3c3b196a2d9e2efd1a2f7b82d304ddb93830fa0c
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-01-31 09:23:30 +0900

    [Gold IV] Title: N-Queen, Time: 30020 ms, Memory: 211852 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,30 +1,24 @@
 n = int(input())
 answer = 0
-rows = [0] * n
+row = [0] * n
 
-# 현재 놓은 퀸 자리가 유효한지 체크
-def is_valid(r):
-    for i in range(r):
-        # 세로 체크
-        if rows[i] == rows[r]:
-            return False
-        # 대각선 체크
-        if abs(r - i) == abs(rows[r] - rows[i]):
+def is_promising(depth, i):
+    for k in range(depth):
+        if row[depth] == row[k] or abs(row[k] - i) == depth - k:
             return False
     return True
 
-# r행에 퀸을 놓아보기
-def put_queen(r):
+def dfs(depth):
     global answer
-    if r == n:
+
+    if depth == n:
         answer += 1
         return
-    
+
     for i in range(n):
-        rows[r] = i
-        if is_valid(r):
-            # 해당 자리에 놓을 수 있다면 다음행으로!
-            put_queen(r + 1)
-    
-put_queen(0)
+        row[depth] = i
+        if is_promising(depth, i):
+            dfs(depth + 1)
+
+dfs(0)
 print(answer)

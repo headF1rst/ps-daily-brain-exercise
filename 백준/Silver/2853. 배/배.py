f6ee60ead95aa32f4ac457edc9410cf6efa5953a
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-02-08 12:45:39 +0900

    [Silver I] Title: 배, Time: 268 ms, Memory: 115108 KB -BaekjoonHub

--- 백준/Silver/2853. 배/배.py
+++ 백준/Silver/2853. 배/배.py
@@ -2,21 +2,17 @@ n = int(input())
 days = []
 for _ in range(n):
     d = int(input())
-    days.append(d)
+    days.append(d - 1)
 
 ships = []
 for i in range(1, n):
-    ships.append(days[i] - 1)
-
-visited = [False] * (n - 1)
-cnt, checked = 0, 0
-for i in range(n - 1):
-    if not visited[i]:
-        if checked == n - 1:
+    if not ships:
+        ships.append(days[i])
+        continue
+    for ship in ships:
+        if days[i] % ship == 0:
             break
-        cnt += 1
-        for j in range(n - 1):
-            if not visited[j] and ships[j] % ships[i] == 0:
-                visited[j] = True
-                checked += 1
-print(cnt)
+    else:
+        ships.append(days[i])
+
+print(len(ships))

8781bf9efc8ab3337af99e5f45e07180c7a90cb6
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-02-25 16:27:38 +0900

    [Platinum V] Title: 집배원 한상덕, Time: 1556 ms, Memory: 121656 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
@@ -4,12 +4,13 @@ from collections import deque
 n = int(input())
 G = [list(input()) for _ in range(n)]
 h = [list(map(int, input().split())) for _ in range(n)]
-houses = 0
-fatigue = set()
 INF = math.inf
 answer = INF
 dxs = [1, -1, 0, 0, 1, -1, 1, -1]
-dys = [0, 0, 1, -1, -1, 1, 1, -1]
+dys = [0, 0, 1, -1, 1, -1, -1, 1]
+fatigues = set()
+s, e = 0, 0
+houses = 0
 
 for i in range(n):
     for j in range(n):
@@ -17,19 +18,18 @@ for i in range(n):
             sx, sy = i, j
         if G[i][j] == 'K':
             houses += 1
-        fatigue.add(h[i][j])
+        fatigues.add(h[i][j])
 
-fatigue = list(sorted(fatigue))
-s, e = 0, 0
-len_f = len(fatigue)
+fatigues = sorted(fatigues)
+len_f = len(fatigues)
 
 def in_boundary(nx, ny):
     return 0 <= nx < n and 0 <= ny < n
 
 def bfs(i, j):
+    q = deque([(i, j)])
     visited = [[False] * n for _ in range(n)]
     visited[i][j] = True
-    q = deque([(i, j)])
     cnt = 0
     while q:
         x, y = q.popleft()
@@ -37,20 +37,20 @@ def bfs(i, j):
             nx = x + dx
             ny = y + dy
             if in_boundary(nx, ny) and not visited[nx][ny]:
-                if fatigue[s] <= h[nx][ny] <= fatigue[e]:
-                    q.append((nx, ny))
+                if fatigues[s] <= h[nx][ny] <= fatigues[e]:
                     visited[nx][ny] = True
+                    q.append((nx, ny))
                     if G[nx][ny] == 'K':
                         cnt += 1
     return cnt
 
+
 while s < len_f:
     cnt = 0
-    if fatigue[s] <= h[sx][sy] <= fatigue[e]:
+    if fatigues[s] <= h[sx][sy] <= fatigues[e]:
         cnt = bfs(sx, sy)
-
     if cnt == houses:
-        answer = min(answer, fatigue[e] - fatigue[s])
+        answer = min(answer, fatigues[e] - fatigues[s])
         s += 1
     elif e + 1 < len_f:
         e += 1

6d2db4684cc03e78715fef116eda131df8f2b58a
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-02-28 14:30:37 +0900

    [unrated] Title: 인사고과, Time: 145.11 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.py
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.py
@@ -1,20 +1,16 @@
 def solution(scores):
-    target = scores[0]
-    target_score = sum(target)
-    scores.sort(key=lambda x: (-x[0], x[1]))
     answer = 1
-    Max = 0
-    Min = int(1e9)
-    
+
+    target = scores[0]
+    target_score = sum(scores[0])
+    scores.sort(key=lambda x: (-x[0], x[1]))
+
+    threshold = 0
     for score in scores:
         if target[0] < score[0] and target[1] < score[1]:
             return -1
-        if Max > score[1] and Min > score[0]:
-            continue
-            
-        if target_score < score[0] + score[1]:
-            answer += 1
-        Max = max(Max, score[1])
-        Min = min(Min, score[0])
-    
+        if threshold <= score[1]:
+            if target_score < score[0] + score[1]:
+                answer += 1
+            threshold = score[1]
     return answer

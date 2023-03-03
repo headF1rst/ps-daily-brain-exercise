39a76847e5fe6b887b97702b3971f28d05a7ef46
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-03-03 22:10:05 +0900

    [level 4] Title: 미로 탈출, Time: 236.02 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/lv4/81304. 미로 탈출/미로 탈출.py
+++ 프로그래머스/lv4/81304. 미로 탈출/미로 탈출.py
@@ -1,19 +1,22 @@
 from heapq import heappush, heappop
 import math
+
 INF = math.inf
 
-def dijk(n, G, src, dst, traps):
-    pq = [(0, src, 0)]
+def dijk(n, src, dst, G, traps):
     visited = [[False] * (1 << len(traps)) for _ in range(n + 1)]
+    pq = [(0, src, 0)]
     
     while pq:
-        c, cur, state = heappop(pq)
+        cost, cur, state = heappop(pq)
         if cur == dst:
-            return c
+            return cost
+        
         if not visited[cur][state]:
             visited[cur][state] = True
-            curTrapped = False
             trapped = {}
+            cur_trapped = False
+            
             for i in range(len(traps)):
                 bit = 1 << i
                 if state & bit:
@@ -25,29 +28,25 @@ def dijk(n, G, src, dst, traps):
                     if traps[i] == cur:
                         state |= bit
                         trapped[traps[i]] = True
-                        curTrapped = True
+                        cur_trapped = True
             
             for next_node in range(1, n + 1):
                 if next_node == cur:
                     continue
                 next_trapped = True if next_node in trapped else False
-                if curTrapped == next_trapped:
+                
+                if cur_trapped == next_trapped:
                     if G[cur][next_node] != INF:
-                        heappush(pq, (c + G[cur][next_node], next_node, state))
+                        heappush(pq, (cost + G[cur][next_node], next_node, state))
                 else:
                     if G[next_node][cur] != INF:
-                        heappush(pq, (c + G[next_node][cur], next_node, state))
+                        heappush(pq, (cost + G[next_node][cur], next_node, state))
                     
 
 def solution(n, start, end, roads, traps):
-    answer = 0
     G = [[INF] * (n + 1) for _ in range(n + 1)]
-    
-    for i in range(1, n + 1):
-        G[i][i] = 0
-    
     for road in roads:
         a, b, c = road
-        if c < G[a][b]:
-            G[a][b] = c
-    return dijk(n, G, start, end, traps)
+        G[a][b] = min(G[a][b], c)
+    
+    return dijk(n, start, end, G, traps)

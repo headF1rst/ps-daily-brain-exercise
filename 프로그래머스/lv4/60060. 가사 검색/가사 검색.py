1034ece94a46d3cc0e41fb0c87d8ed57f35c8038
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-03-15 21:59:44 +0900

    [level 4] Title: 가사 검색, Time: 3.67 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
+def count_by_range(lst, s, e):
+    return bisect_right(lst, e) - bisect_left(lst, s)
+
+def solution(words, queries):
+    answer = []
+    cands = defaultdict(list)
+    reverse_cands = defaultdict(list)
+    
+    for word in words:
+        cands[len(word)].append(word)
+        reverse_cands[len(word)].append(word[::-1])
+        
+    for cand in cands.values():
+        cand.sort()
+    for cand in reverse_cands.values():
+        cand.sort()
+    
+    for query in queries:
+        if query[0] == '?':
+            lst = reverse_cands[len(query)]
+            s, e = query[::-1].replace('?', 'a'), query[::-1].replace('?', 'z')
+        else:
+            lst = cands[len(query)]
+            s, e = query.replace('?', 'a'), query.replace('?', 'z')
+        answer.append(count_by_range(lst, s, e))
+    
+    return answer

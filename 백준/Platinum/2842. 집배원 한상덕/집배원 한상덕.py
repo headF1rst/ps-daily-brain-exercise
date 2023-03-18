9ab38890406bbc166465c937440dbcb7e03231f5
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-03-18 18:06:52 +0900

    [Platinum V] Title: 집배원 한상덕, Time: 1152 ms, Memory: 121516 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
@@ -3,33 +3,34 @@ from collections import deque
 
 n = int(input())
 G = [list(input()) for _ in range(n)]
-h = [list(map(int, input().split())) for _ in range(n)]
+costs = [list(map(int, input().split())) for _ in range(n)]
+fatigues = set()
+fatigue_cnt = 0
 INF = math.inf
 answer = INF
+s, e = 0, 0
+
 dxs = [1, -1, 0, 0, 1, -1, 1, -1]
 dys = [0, 0, 1, -1, 1, -1, -1, 1]
-fatigues = set()
-s, e = 0, 0
-houses = 0
 
 for i in range(n):
     for j in range(n):
         if G[i][j] == 'P':
             sx, sy = i, j
-        if G[i][j] == 'K':
-            houses += 1
-        fatigues.add(h[i][j])
+        elif G[i][j] == 'K':
+            fatigue_cnt += 1
+        fatigues.add(costs[i][j])
 
-fatigues = sorted(fatigues)
 len_f = len(fatigues)
+fatigues = list(sorted(fatigues))
 
 def in_boundary(nx, ny):
     return 0 <= nx < n and 0 <= ny < n
 
-def bfs(i, j):
-    q = deque([(i, j)])
+def bfs(sx, sy):
     visited = [[False] * n for _ in range(n)]
-    visited[i][j] = True
+    q = deque([(sx, sy)])
+    visited[sx][sy] = True
     cnt = 0
     while q:
         x, y = q.popleft()
@@ -37,24 +38,22 @@ def bfs(i, j):
             nx = x + dx
             ny = y + dy
             if in_boundary(nx, ny) and not visited[nx][ny]:
-                if fatigues[s] <= h[nx][ny] <= fatigues[e]:
-                    visited[nx][ny] = True
+                visited[nx][ny] = True
+                if fatigues[s] <= costs[nx][ny] <= fatigues[e]:
                     q.append((nx, ny))
                     if G[nx][ny] == 'K':
                         cnt += 1
     return cnt
 
-
 while s < len_f:
     cnt = 0
-    if fatigues[s] <= h[sx][sy] <= fatigues[e]:
+    if fatigues[s] <= costs[sx][sy] <= fatigues[e]:
         cnt = bfs(sx, sy)
-    if cnt == houses:
+    if cnt == fatigue_cnt:
         answer = min(answer, fatigues[e] - fatigues[s])
         s += 1
     elif e + 1 < len_f:
         e += 1
     else:
         break
-
 print(answer)

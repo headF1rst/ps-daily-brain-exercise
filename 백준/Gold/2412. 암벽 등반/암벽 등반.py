186d252f9b8b39293a743226f070431c72a4f407
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-04-05 09:01:38 +0900

    [Gold IV] Title: 암벽 등반, Time: 300 ms, Memory: 122388 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.py
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.py
@@ -1,24 +1,28 @@
 from collections import deque
+
 n, T = map(int, input().split())
-dots = set()
-answer = -1
+halls = set()
+flag = False
 
 for _ in range(n):
     x, y = map(int, input().split())
-    dots.add((x, y))
+    halls.add((x, y))
 
 q = deque([(0, 0, 0)])
 while q:
     x, y, cnt = q.popleft()
     if y == T:
-        answer = cnt
+        flag = True
+        print(cnt)
         break
+
     for dx in range(-2, 3):
         for dy in range(-2, 3):
             nx = x + dx
             ny = y + dy
-            if (nx, ny) in dots:
+            if (nx, ny) in halls:
                 q.append((nx, ny, cnt + 1))
-                dots.remove((nx, ny))
+                halls.remove((nx, ny))
 
-print(answer)
+if not flag:
+    print(-1)

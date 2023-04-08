56b092f356d1a9154cfa72d7f2e88b22ee1671d0
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-04-08 22:23:10 +0900

    [Gold IV] Title: 감시, Time: 1524 ms, Memory: 159520 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,50 +1,54 @@
+import math
 from copy import deepcopy
 
 n, m = map(int, input().split())
 G = [list(map(int, input().split())) for _ in range(n)]
-direction = [[], [[0], [1], [2], [3]], [[0, 2], [1, 3]], [[0, 1], [1, 2], [2, 3], [3, 0]],
-             [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]], [[0, 1, 2, 3]]]
-dxs = [1, 0, -1, 0]
-dys = [0, 1, 0, -1]
 cctvs = []
 cctv_cnt = 0
-answer = int(1e9)
+INF = math.inf
+answer = INF
+
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
+directions = [[], [[0], [1], [2], [3]], [[0, 2], [1, 3]], [[0, 1], [1, 2], [2, 3], [3, 0]],
+              [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]], [[0, 1, 2, 3]]]
 
 for i in range(n):
     for j in range(m):
-        if 0 < G[i][j] < 6:
+        if 1 <= G[i][j] <= 5:
             cctvs.append((i, j, G[i][j]))
             cctv_cnt += 1
 
 def in_boundary(nx, ny):
     return 0 <= nx < n and 0 <= ny < m
 
-def watch(x, y, direct, graph):
-    for d in direct:
-        nx, ny = x, y
+def watch(x, y, graph, direction):
+    for d in direction:
+        nx = x
+        ny = y
         while True:
             nx += dxs[d]
             ny += dys[d]
             if not in_boundary(nx, ny) or graph[nx][ny] == 6:
                 break
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = '#'
+            graph[nx][ny] = '#'
 
 def dfs(depth, graph):
     global answer
+
     if depth == cctv_cnt:
-        c = 0
+        cnt = 0
         for i in graph:
-            c += i.count(0)
-        answer = min(answer, c)
+            cnt += i.count(0)
+        answer = min(answer, cnt)
         return
 
     x, y, num = cctvs[depth]
-    tmp_G = deepcopy(graph)
-    for direct in direction[num]:
-        watch(x, y, direct, tmp_G)
-        dfs(depth + 1, tmp_G)
-        tmp_G = deepcopy(graph)
+    copy_graph = deepcopy(graph)
+    for direction in directions[num]:
+        watch(x, y, copy_graph, direction)
+        dfs(depth + 1, copy_graph)
+        copy_graph = deepcopy(graph)
 
 dfs(0, G)
 print(answer)

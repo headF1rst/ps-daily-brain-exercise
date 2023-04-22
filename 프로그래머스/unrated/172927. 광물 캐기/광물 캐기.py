5f7d3fc4d1424bfb7e7fa223a4eb46de6af3a5a1
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-04-22 19:15:28 +0900

    [unrated] Title: 광물 캐기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
@@ -1,19 +1,20 @@
 def solution(picks, minerals):
-    
-    def list_divided(target_list, n):
-        return [target_list[i: i + n] for i in range(0, len(target_list), n)]
-    
-    minerals_divided = list_divided(minerals, 5)
+    answer = 0
     costs = []
     
-    for section in minerals_divided:
+    def divide(minerals, off_set):
+        return [minerals[i: i + off_set] for i in range(0, len(minerals), off_set)]
+        
+    divided_minerals = divide(minerals, 5)
+    
+    for mineral in divided_minerals:
         cost = [0, 0, 0]
-        for mineral in section:
-            if mineral == 'diamond':
+        for m in mineral:
+            if m == 'diamond':
                 cost[0] += 1
                 cost[1] += 5
                 cost[2] += 25
-            elif mineral == 'iron':
+            elif m == 'iron':
                 cost[0] += 1
                 cost[1] += 1
                 cost[2] += 5
@@ -22,23 +23,23 @@ def solution(picks, minerals):
                 cost[1] += 1
                 cost[2] += 1
         costs.append(cost)
-    
     while len(costs) > sum(picks):
         costs.pop()
     
-    costs_sorted = sorted(costs, key=lambda x: x[2], reverse=True)
-    cost_integrated = 0
-    for cost in costs_sorted:
+    costs.sort(key=lambda x: x[2], reverse=True)
+    
+    for cost in costs:
         if picks[0] > 0:
-            cost_integrated += cost[0]
             picks[0] -= 1
+            answer += cost[0]
             continue
         if picks[1] > 0:
-            cost_integrated += cost[1]
             picks[1] -= 1
+            answer += cost[1]
             continue
         if picks[2] > 0:
-            cost_integrated += cost[2]
             picks[2] -= 1
-    
-    return cost_integrated
+            answer += cost[2]
+            continue
+            
+    return answer

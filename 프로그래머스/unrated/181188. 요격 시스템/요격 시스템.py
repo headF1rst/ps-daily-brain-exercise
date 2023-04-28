62a53d45398bfed800022dfbba3a7a264cafd033
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-04-28 09:37:01 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,10 +1,10 @@
 def solution(targets):
     answer = 0
     targets.sort(key=lambda x: x[1])
-    s, e = 0, 0
     
+    s, e = 0, 0
     for target in targets:
-        if target[0] >= e:
+        if e <= target[0]:
             answer += 1
             e = target[1]
     

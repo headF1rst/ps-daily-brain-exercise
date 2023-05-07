7859839b24f85b40ea3240eca8e2d6ad6169e100
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-05-07 21:00:31 +0900

    [unrated] Title: 억억단을 외우자, Time: 9595.91 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
+++ 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
@@ -1,21 +1,20 @@
 def solution(e, starts):
-    answer = []
-    divisor = [1] * (e + 1)
-    check = [0] * (e + 1)
+    appeared = [0] * (e + 1)
+    numbs = [1] * (e + 1)
     max_num = 0
     
-    for i in range(2, e + 1):
+    for i in range(1, e + 1):
         for j in range(i, e + 1):
             tmp = i * j
             if tmp > e:
                 break
-            divisor[tmp] += 1 if i == j else 2
+            appeared[tmp] += 1 if i == j else 2
     
     for i in range(e, 0, -1):
-        if divisor[i] >= max_num:
-            max_num = divisor[i]
-            check[i] = i
+        if appeared[i] >= max_num:
+            numbs[i] = i
+            max_num = appeared[i]
         else:
-            check[i] = check[i + 1]
+            numbs[i] = numbs[i + 1]
     
-    return [check[start] for start in starts]
+    return [numbs[start] for start in starts]

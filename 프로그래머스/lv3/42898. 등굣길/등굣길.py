24c1d5063162e9a4e4c50b282ec0f748d01b880d
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-06-09 11:54:32 +0900

    [level 3] Title: 등굣길, Time: 2.19 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,17 @@
+import math
+def solution(m, n, puddles):
+    puddles = [[b, a] for a, b in puddles]
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
+    MOD = 1000000007
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1:
+                continue
+            if [i, j] in puddles:
+                dp[i][j] = 0
+                continue
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD
+    
+    return dp[n][m]
